9ece929d783cf665cbcef6cb0727fb9e8b89838b
geniusvjr <geniusvjr@gmail.com>
2016-10-06 20:20:54 +0800

    finish item

---
+++ app/src/main/java/com/francis/vbaseadapter/bean/Bean.java
@@ -0,0 +1,57 @@
+package com.francis.vbaseadapter.bean;
+
+/**
+ * @author taoc @ Zhihu Inc.
+ * @since 10-06-2016
+ */
+
+public class Bean {
+
+	private String title;
+	private String desc;
+	private String time;
+	private String phone;
+
+	public Bean(){
+
+	}
+
+	public Bean(String title, String desc, String time, String phone) {
+		this.title = title;
+		this.desc = desc;
+		this.time = time;
+		this.phone = phone;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+}
